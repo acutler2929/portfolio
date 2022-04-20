90b6e568ce342b6bdb76f5eec116de2c4cf023f4
Alice Cutler <acutler0451@gmail.com>
2022-04-20 06:44:50 -0400

    fixed font responsiveness

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const displayModal = function () {
 	const btn4 = document.getElementById('project-clickable-four');
 
 	// Get the <span> element that closes the modal
-	const span = document.getElementsByClassName('close')[0];
+	const closeSpan = document.getElementsByClassName('close')[0];
 
 	// When the user clicks on the button, open the modal
 	btn1.onclick = function () {
@@ -31,7 +31,7 @@ const displayModal = function () {
 	};
 
 	// When the user clicks on <span> (x), close the modal
-	span.onclick = function () {
+	closeSpan.onclick = function () {
 		projectModal[0].style.display = 'none';
 	};
 
